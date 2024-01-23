575c3d07ccc9ecf3bf12cde037661127300a52a1
Andre-Viewqwest <156410769+Andre-Viewqwest@users.noreply.github.com>
2024-01-23 11:11:42 +0800

    updated qr

--- app/Http/Controllers/QRCodeInvoiceController.php
+++ app/Http/Controllers/QRCodeInvoiceController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
+<<<<<<< Updated upstream
 use Illuminate\Support\Facades\Response;
+=======
+use Illuminate\Support\Facades\Storage;
+>>>>>>> Stashed changes
 
 class QRCodeInvoiceController extends Controller
 {
@@ -13,14 +17,47 @@ class QRCodeInvoiceController extends Controller
         try {
             $molpayApiKey = env('MOLPAY_API');
 
+            // Concatenate your API key and $id to create the link
             $link = $molpayApiKey . $id;
 
+            // Generate QR code
             $qrCode = QrCode::size(300)->generate($link);
 
-            return view('qrCodeView', compact('qrCode'));
-        } catch (\Exception $e) {
+            // Generate a unique filename for the QR code image
+            $filename = 'qrcode_' . $id . '.png';
+
+            // Save the image to the specified file path
+            $filePath = 'public/' . $filename;
+            Storage::put($filePath, $qrCode);
+
+            return 'QR code generated and saved successfully!';
+        }catch (\Exception $e) {
             // Log or handle the exception
             return response('Error generating QR code', 500);
         }
+
+
+
+        // try {
+        //     $molpayApiKey = env('MOLPAY_API');
+
+        //     // Concatenate your API key and $id to create the link
+        //     $link = $molpayApiKey . $id;
+
+        //     // Generate QR code
+        //     $qrCode = QrCode::size(300)->generate($link);
+
+        //     // Generate a unique filename for the QR code image
+        //     $filename = 'qrcode_' . $id . '.png';
+
+        //     // Save the image to the specified file path
+        //     $filePath = 'public/' . $filename;
+        //     Storage::put($filePath, $qrCode);
+
+        //     return 'QR code generated and saved successfully!';
+        // }catch (\Exception $e) {
+        //     // Log or handle the exception
+        //     return response('Error generating QR code', 500);
+        // }
     }
 }

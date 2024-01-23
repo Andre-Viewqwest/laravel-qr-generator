9d1a42c6c6a76225f510d786b2c631ff17f09375
Andre-Viewqwest <156410769+Andre-Viewqwest@users.noreply.github.com>
2024-01-23 14:15:23 +0800

    Update QRCodeInvoiceController.php

--- app/Http/Controllers/QRCodeInvoiceController.php
+++ app/Http/Controllers/QRCodeInvoiceController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Illuminate\Support\Facades\Storage;
 
 class QRCodeInvoiceController extends Controller
 {
@@ -21,19 +20,38 @@ class QRCodeInvoiceController extends Controller
             // Convert the QR code image to base64
             $base64Image = base64_encode($qrCode);
 
-            // Generate a unique filename for the QR code image
-            $filename = 'qrcode_' . $id . '.txt'; // Use .txt extension for base64 data
-
-            // Save the base64-encoded string to the specified file path in the local storage
-            $filePath = 'public/' . $filename;
-            Storage::put($filePath, $base64Image);
-
             // Return the path to the saved file
-            return response()->json(['file_path' => $filePath, 'message' => 'Base64 data saved as a file successfully']);
+            return response()->json(['file_path' => $base64Image,]);
         }catch (\Exception $e) {
-            // Log or handle the exception
             return response('Error generating QR code', 500);
         }
+
+        // base 64 save in localstorage
+        // try {
+        //     $molpayApiKey = env('MOLPAY_API');
+
+        //     // Concatenate your API key and $id to create the link
+        //     $link = $molpayApiKey . $id;
+
+        //     // Generate QR code
+        //     $qrCode = QrCode::size(300)->generate($link);
+
+        //     // Convert the QR code image to base64
+        //     $base64Image = base64_encode($qrCode);
+
+        //     // Generate a unique filename for the QR code image
+        //     $filename = 'qrcode_' . $id . '.txt'; // Use .txt extension for base64 data
+
+        //     // Save the base64-encoded string to the specified file path in the local storage
+        //     $filePath = 'public/' . $filename;
+        //     Storage::put($filePath, $base64Image);
+
+        //     // Return the path to the saved file
+        //     return response()->json(['file_path' => $filePath, 'message' => 'Base64 data saved as a file successfully']);
+        // }catch (\Exception $e) {
+        //     // Log or handle the exception
+        //     return response('Error generating QR code', 500);
+        // }
 
         // Generate png
         // try {
@@ -57,9 +75,5 @@ class QRCodeInvoiceController extends Controller
         //     // Log or handle the exception
         //     return response('Error generating QR code', 500);
         // }
-    }
-
-    public function showqr(){
-
     }
 }
